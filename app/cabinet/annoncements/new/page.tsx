5710300fe37d0f2392257d73e03d0eef0ad90079
kzBooking <kz.booking@mail.ru>
2024-03-09 01:56:00 +0500

    connected with db

---
+++ app/cabinet/annoncements/new/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Новое обьявление",
+  description: "booking.kz | booking.kz",
+};
+
+function AnnoncementNew() {
+  return <div>AnnoncementNew</div>;
+}
+
+export default AnnoncementNew;
